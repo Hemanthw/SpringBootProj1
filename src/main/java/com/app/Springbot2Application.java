e4e68056970199f08e70de1b1307904e201f62e3
lenovo <tech.amolw@gmail.com>
2021-05-01 11:09:52 +0530

    thirst commit

--- src/main/java/com/app/Springbot2Application.java
+++ src/main/java/com/app/Springbot2Application.java
@@ -8,6 +8,7 @@ public class Springbot2Application {
 
 	public static void main(String[] args) {
 		//welcome to first app
+		//lets check your branches on git
 		SpringApplication.run(Springbot2Application.class, args);
 	}
 
