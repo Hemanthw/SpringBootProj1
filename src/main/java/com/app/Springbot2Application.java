af95d91f0c93fbf37a17f2d5adbbb6006bd7d005
lenovo <tech.amolw@gmail.com>
2021-05-01 11:08:50 +0530

    second commit

--- src/main/java/com/app/Springbot2Application.java
+++ src/main/java/com/app/Springbot2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springbot2Application {
 
 	public static void main(String[] args) {
+		//welcome to first app
 		SpringApplication.run(Springbot2Application.class, args);
 	}
 
